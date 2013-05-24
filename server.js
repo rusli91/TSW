e3a846ea16d61f81c702dd88a393d4b2d8e9da8c
Tomasz Żubertowski <mastah998@gmail.com>
2013-05-24 21:21:47 +0200

    Socket.io początek implementacji

---
+++ server.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var http = require('http');
+var path = require('path');
+
+var osm = express();
+osm.configure(function() {
+    osm.set('port', process.env.PORT || 3000);
+    osm.use(express.favicon());
+    osm.use(express.logger('dev'));
+    osm.use(less({
+        src: __dirname + '/public',
+        compress: true
+    }));
+    osm.use(express.static(path.join(__dirname, 'client')));
+});
+var server = http.createServer(osm).listen(osm.get('port'), function() {
+    console.log("Serwer http działa na porcie: " + osm.get('port'));
+});
