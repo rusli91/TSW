597871e00b2278c2378de5f096ebc55c9e16bc51
Karen <karen.orm@vp.pl>
2013-06-09 13:07:54 +0200

    Revert "nowy komit"
    This reverts commit edc4f2bdb2f9525a7eaa221dec949929ebda5282.

--- server.js
+++ server.js
@@ -16,4 +16,4 @@ osm.configure(function() {
 });
 var server = http.createServer(osm).listen(osm.get('port'), function() {
     console.log("Serwer http działa na porcie: " + osm.get('port'));
-});  
+});
